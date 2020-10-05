57ca091eb4532c682ca67c6705bf0deab6e66bf4
BlaKe00786 <sameershaikh403@gmail.com>
2020-10-05 22:26:31 +0530

    [Sameer]Add: Added the welcome File and the project.

---
+++ UserRegistrationProject/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace UserRegistrationProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
