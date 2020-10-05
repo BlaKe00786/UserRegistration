42ebb7cf0d6d58338ca4af89e7081e6b95b3e582
BlaKe00786 <sameershaikh403@gmail.com>
2020-10-05 22:55:15 +0530

    [Sameer]Refactor: User First Name Validation.

--- UserRegistrationProject/Program.cs
+++ UserRegistrationProject/Program.cs
@@ -1,11 +1,24 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 namespace UserRegistrationProject
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string firstName;
+            var regex = @"^[A-Z][a-z]{2,18}$";
+            Console.WriteLine("Enter First Name: ");
+            firstName = Console.ReadLine();
+            var match = Regex.Match(firstName, regex);
+            if (match.Success)
+            {
+                Console.WriteLine("Valid First Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid First Name");
+            }
         }
     }
 }
