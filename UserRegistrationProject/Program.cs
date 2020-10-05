5b1628bfa4d0e201177d31a2ee3601c9cddded3e
BlaKe00786 <sameershaikh403@gmail.com>
2020-10-05 23:02:49 +0530

    [Sameer]Refactor: Last Name VAlidation.

--- UserRegistrationProject/Program.cs
+++ UserRegistrationProject/Program.cs
@@ -6,18 +6,29 @@ namespace UserRegistrationProject
     {
         static void Main(string[] args)
         {
-            string firstName;
+            string firstName,lastName;
             var regex = @"^[A-Z][a-z]{2,18}$";
             Console.WriteLine("Enter First Name: ");
             firstName = Console.ReadLine();
-            var match = Regex.Match(firstName, regex);
-            if (match.Success)
+            Console.WriteLine("Enter Last Name: ");
+            lastName = Console.ReadLine();
+            var matchFirstName = Regex.Match(firstName, regex);
+            if (matchFirstName.Success)
             {
                 Console.WriteLine("Valid First Name");
             }
             else
             {
                 Console.WriteLine("Invalid First Name");
+            }
+            var matchLastName = Regex.Match(lastName, regex);
+            if (matchLastName.Success)
+            {
+                Console.WriteLine("Valid Last Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Last Name");
             }
         }
     }
