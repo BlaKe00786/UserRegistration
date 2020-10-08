d3d9d3be4ac66df1f2264a1006d23d4149e01216
BlaKe00786 <sameershaikh403@gmail.com>
2020-10-08 20:32:40 +0530

    [Sameer]Refactor:Added Validation on password to check atleast 1 uppercase.

--- UserRegistrationProject/Program.cs
+++ UserRegistrationProject/Program.cs
@@ -7,10 +7,10 @@ namespace UserRegistrationProject
         static void Main(string[] args)
         {
             string firstName,lastName,emailID,mobileNum,password;
-            var regexName = @"^[A-Z][a-z]{2,18}$";
-            var regexEmailID = @"^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*[@][0-9a-zA-Z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
-            var regexMobile = @"[1-9]{1}[0-9]{1,2}[ ]{1}[6-9]{1}[0-9]{9}";
-            var regexPassword = @"[0-9a-zA-Z]{8,}";
+            var regexName = "^[A-Z][a-z]{2,18}$";
+            var regexEmailID = "^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*[@][0-9a-zA-Z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
+            var regexMobile = "^[1-9]{1}[0-9]{1,2}[ ]{1}[6-9]{1}[0-9]{9}$";
+            var regexPassword = "^([a-zA-Z]*[A-Z]+[a-zA-Z]*).{8,}$";
             Console.WriteLine("Enter First Name: ");
             firstName = Console.ReadLine();
             checkIfValid(regexName,firstName);
