c4493530bf42c5cf9cee0e22158ab7b3d39d439c
BlaKe00786 <sameershaikh403@gmail.com>
2020-10-08 20:46:45 +0530

    [Sameer]Refactor: added password validation to include atleast 1 numeric character.

--- UserRegistrationProject/Program.cs
+++ UserRegistrationProject/Program.cs
@@ -10,7 +10,7 @@ namespace UserRegistrationProject
             var regexName = "^[A-Z][a-z]{2,18}$";
             var regexEmailID = "^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*[@][0-9a-zA-Z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
             var regexMobile = "^[1-9]{1}[0-9]{1,2}[ ]{1}[6-9]{1}[0-9]{9}$";
-            var regexPassword = "^([a-zA-Z]*[A-Z]+[a-zA-Z]*).{8,}$";
+            var regexPassword = "(?=[A-Za-z0-9]+$)^(?=.*[A-Z])(?=.*[0-9])(?=.{8,}).*$";
             Console.WriteLine("Enter First Name: ");
             firstName = Console.ReadLine();
             checkIfValid(regexName,firstName);
