97f6b98c64aa7925b05c108f682564ed85cf6a77
BlaKe00786 <sameershaikh403@gmail.com>
2020-10-12 15:48:51 +0530

    [Sameer]Refactor:Valid password Rule 4

--- UserRegistrationProject/Program.cs
+++ UserRegistrationProject/Program.cs
@@ -10,20 +10,20 @@ namespace UserRegistrationProject
             var regexName = "^[A-Z][a-z]{2,18}$";
             var regexEmailID = "^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*[@][0-9a-zA-Z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
             var regexMobile = "^[1-9]{1}[0-9]{1,2}[ ]{1}[6-9]{1}[0-9]{9}$";
-            var regexPassword = "(?=[A-Za-z0-9]+$)^(?=.*[A-Z])(?=.*[0-9])(?=.{8,}).*$";
+            var regexPassword = "(?=.*?[._+-@#&*$])(?=.*[A-Z])(?=.*[0-9])([0-9a-zA-z]{7,})";
             Console.WriteLine("Enter First Name: ");
             firstName = Console.ReadLine();
             checkIfValid(regexName,firstName);
-            Console.WriteLine("Enter Last Name: ");
+            Console.WriteLine("Enter Last Name : ");
             lastName = Console.ReadLine();
             checkIfValid(regexName, lastName);
-            Console.WriteLine("Enter Email ID: ");
+            Console.WriteLine("Enter Email ID : ");
             emailID = Console.ReadLine();
             checkIfValid(regexEmailID, emailID);
-            Console.WriteLine("Enter Mobile Number: ");
+            Console.WriteLine("Enter Mobile Number : ");
             mobileNum = Console.ReadLine();
             checkIfValid(regexMobile, mobileNum);
-            Console.WriteLine("Enter Password: ");
+            Console.WriteLine("Enter Password : ");
             password = Console.ReadLine();
             checkIfValid(regexPassword, password);
         }
